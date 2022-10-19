0c9e7d35f912c7260c2a150e77fcd8efaff393c1
ozy <fahrurozy182@gmail.com>
2022-10-19 11:58:38 +0700

    update node

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -4,7 +4,7 @@
             <form wire:submit.prevent="submit" class="space-y-6">
                 <h1
                     class="text-4xl mt-6 tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-                    Login yah
+                    Login yah yah
                 </h1>
                 <p class="text-lg mt-2 text-gray-600">Enter your imagination</p>
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
